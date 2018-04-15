4906be72bbb61d91109bd1418853724bb156ab35
Ben <emersion.space@gmail.com>
2018-04-15 18:35:43 +0100

    2nd scenario

--- ui-tests/tests/specs/PageObject.js
+++ ui-tests/tests/specs/PageObject.js
@@ -11,8 +11,10 @@ var PageObject = function()
     this.searchField = element(by.css('#query'));
     this.searchButton = element(by.name('commit'));
     this.searchSignInResult = element(by.linkText("How do I ‘Sign In’ to the 4G Clinical Help Center?"));
-    //this.searchSignInResultTitle = element(by.xpath("//h1")).getText().matches("^How do I ‘Sign In’ to the 4G Clinical Help Center[\\s\\S]$");
-
+    this.searchCoBrowseResult = element(by.linkText("What is CoBrowse, how to use CoBrowse with a 4G Client Excellence Associate"));
+  
+    this.ticketButton = element(by.xpath('//li[3]/a/span/i'));
+    this.helpDropdown = element(by.linkText("-"));
 
     this.signInPassword = element(by.id('user_password'));
 
--- ui-tests/tests/specs/ui_test_spec.js
+++ ui-tests/tests/specs/ui_test_spec.js
@@ -15,22 +15,26 @@ describe('4G Clinical help center', function () {
         page.searchSignInResult.click()
         expect(browser.getCurrentUrl()).toEqual('https://support.4gclinical.com/hc/en-us/articles/360000850247-How-do-I-Sign-In-to-the-4G-Clinical-Help-Center-');  
     });
-    // it('should launch sign in window after clicking sign in button', function () {  
-    //     page.signIn.click();
-    //     browser.sleep('4000');  
-    //     //element(by.input('#user_email').click());
-    //     expect(page.signInEmail.isDisplayed());
-    //     //page.signInEmail.click();
-    //     // page.signInEmail.sendKeys('write first protractor test');
-    //     browser.sleep('4000');  
-    //     //expect(page.signInEmail.isDisplayed()).toBe(true);             
-    // });      
 
-    // it('should launch sign in window after clicking sign in button', function () {  
-    //     page.signIn.click();
-    //     browser.sleep('4000');  
-    //     page.signUp.click();
-    //     browser.sleep('4000');  
-    //     //expect(page.signInEmail.isDisplayed()).toBe(true);             
-    // });     
+    it('should allow user to submit a support ticket', function () {  
+        page.ticketButton.click();
+        page.helpDropdown.click(); 
+    }); 
+
+
 });
+
+// describe('4G Clinical help center - sign in', function () {
+//     var page;
+
+//     beforeEach(function () {
+//         page = new PageObject();
+//         browser.waitForAngularEnabled(false);
+//         browser.get('https://support.4gclinical.com/hc/en-us');
+//     });
+
+//     it('should launch sign in window after clicking sign in button', function () {  
+//         page.signIn.click();
+//         browser.sleep('4000');     
+//     });     
+// });
