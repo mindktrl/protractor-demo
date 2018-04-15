d37fac552536440d849bcaf202ccffca5cf5f7c3
Ben <emersion.space@gmail.com>
2018-04-15 20:12:27 +0100

    working

--- ui-tests/tests/specs/PageObject.js
+++ ui-tests/tests/specs/PageObject.js
@@ -15,6 +15,12 @@ var PageObject = function()
   
     this.ticketButton = element(by.xpath('//li[3]/a/span/i'));
     this.helpDropdown = element(by.linkText("-"));
+    this.helpDropdownGeneral = element(by.id("352228"));
+    this.helpRequestIssueTypeSelect = element(by.id('request_issue_type_select'));
+    this.helpRequestIssueTitle = element(by.xpath("//h1"));
+    //this.helpSubmit = element(by.xpath("//input[@name='commit'])[2]"));
+    this.helpSubmit = element(by.css('#new_request > footer > input[type="submit"]'));
+    //this.helpCaptcha = element(by.xpath("//form[@id='new_request']/div[16]"));
 
     this.signInPassword = element(by.id('user_password'));
 
--- ui-tests/tests/specs/ui_test_spec.js
+++ ui-tests/tests/specs/ui_test_spec.js
@@ -9,16 +9,26 @@ describe('4G Clinical help center', function () {
         browser.get('https://support.4gclinical.com/hc/en-us');
     });
 
-    it('should display sign in help page when searching for sign in', function () {  
-        page.searchField.click().sendKeys('sign in');
-        page.searchButton.click();
-        page.searchSignInResult.click()
-        expect(browser.getCurrentUrl()).toEqual('https://support.4gclinical.com/hc/en-us/articles/360000850247-How-do-I-Sign-In-to-the-4G-Clinical-Help-Center-');  
-    });
+    // it('should display sign in help page when searching for sign in', function () {  
+    //     page.searchField.click().sendKeys('sign in');
+    //     page.searchButton.click();
+    //     page.searchSignInResult.click()
+    //     expect(browser.getCurrentUrl()).toEqual('https://support.4gclinical.com/hc/en-us/articles/360000850247-How-do-I-Sign-In-to-the-4G-Clinical-Help-Center-');  
+    // });
 
     it('should allow user to submit a support ticket', function () {  
         page.ticketButton.click();
         page.helpDropdown.click(); 
+        page.helpDropdownGeneral.click(); 
+        //expect(page.helpRequestIssueTypeSelect.isDisplayed().toBe(true));
+        //page.helpRequestIssueTypeSelect.click();
+        //page.helpSubmit.click();
+        //expect((page.helpRequestIssueTitle).getText().toEqual('Submit a request'));
+        //page.helpRequestIssueTitle.isPresent();
+        expect(page.helpRequestIssueTitle.getText()).toEqual('Submit a request');
+        //browser.sleep('4000'); 
+        //expect(page.helpSubmit.isDisplayed()).toBe(true);
+        //expect(element(by.xpath("//input[@name='commit'])[2]")).isDisplayed());
     }); 
 
 
